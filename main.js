2af20818fddb7ad875284fe75bcfd463f7b68b58
Ismaël <dabo_i@epitech.eu>
2018-06-14 11:55:25 +0200

    Navbar (Fixes #2)

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// SCROLL TO TOP
+
 document.getElementById('scrolltop').onclick = function() {
 		window.scrollTo(0, 0);
 	};
+
+// STICK NAVBAR
+
+var navbar = document.getElementById('forscroll')
+var sticky = navbar.offsetTop;
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){ 
+   var st = window.pageYOffset || document.documentElement.scrollTop; 
+   if (st < lastScrollTop){
+  		navbar.classList.add("fix")	
+   }
+   else {
+  		navbar.classList.remove("fix")	
+	}
+   lastScrollTop = st <= 0 ? 0 : st;
+}, false);
